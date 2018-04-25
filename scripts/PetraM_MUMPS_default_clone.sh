1d7874717aae054ff417e6b893dece9f567e508a
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-04-25 15:28:30 -0400

    working on MUMPS

---
+++ scripts/PetraM_MUMPS_default_clone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GIT=$(command -v git)
+SRCDIR=${TwoPiRoot}/src
+REPO=${SRCDIR}/PetraM_MUMPS
+
+mkdir -p $SRCDIR
+if [ ! -f $REPO ]; then
+   cd $SRCDIR
+   $GIT clone git@github.com:piScope/PetraM_MUMPS.git $REPO    
+   cd $REPO
+fi

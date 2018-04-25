606a331d55d8530ac49deb58ed958aead004fc65
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-04-25 15:44:40 -0400

    fix parmetis clone

--- scripts/parmetis_default_clone.sh
+++ scripts/parmetis_default_clone.sh
@@ -5,7 +5,7 @@ SRCDIR=${TwoPiRoot}/src
 
 mkdir -p $SRCDIR
 cd $SRCDIR
-wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/parmetis-4.0.3.tar.gz
+wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
 tar -zxvf parmetis-4.0.3.tar.gz
 rm parmetis-4.0.3.tar.gz
 

513d12765a6f9bfbba80c26e62bb8f821f93cc22
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-04-26 19:34:33 -0400

    typo fix

--- scripts/PetraM_Driver_default_build.sh
+++ scripts/PetraM_Driver_default_build.sh
@@ -9,7 +9,7 @@ cp Makefile_templates/Makefile_default Makefile
 mkdir -p etc
 mkdir -p ${TwoPiRoot}/etc
 cp templates/queue_config_${TwoPiDevice} etc/queue_config
-cp templates/slurm_batch_${TwoPiDevice} etc/slurm_batch
+cp templates/slurm_batch_script_${TwoPiDevice} etc/slurm_batch
 
 export PetraM=${TwoPiRoot}
 

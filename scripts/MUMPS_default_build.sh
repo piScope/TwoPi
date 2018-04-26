d0e9543fe934edff5122bf761458a89023ba8dfb
shiraiwa <shiraiwa@psfc.mit.edu>
2018-04-26 14:45:31 -0400

    MUMPS on MIT engaging runs..

--- scripts/MUMPS_default_build.sh
+++ scripts/MUMPS_default_build.sh
@@ -2,16 +2,19 @@
 
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
+REPO=${SRCDIR}/MUMPS_5.1.2
 CMAKE=$(command -v cmake)
 MAKE=$(command -v make)
 
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
 source $SCRIPT
 
-MAKEINC=$(dirname "$0")/../extra/MUMPS/MUMPS_${TwoPiDevice}_PAR_Makefile.inc
+MYPATH=$(realpath "$0")
+echo $MYPATH
 
-cd ${SRCDIR}/MUMPS_5.1.2
-cp $MAKEINC Makefile.inc
+MAKEINC=$(dirname "$MYPATH")/../extra/MUMPS/MUMPS_${TwoPiDevice}_PAR_Makefile.inc
+cp $MAKEINC ${REPO}/Makefile.inc
+cd ${REPO}
 
 $MAKE all
 $MAKE

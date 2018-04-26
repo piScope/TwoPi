5ff3033ef6cf91678cd46141ba4047ab226a4d17
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-04-26 19:30:30 -0400

    working on driver script...

--- scripts/PetraM_Driver_default_build.sh
+++ scripts/PetraM_Driver_default_build.sh
@@ -6,6 +6,9 @@ MAKE=$(command -v make)
 
 cd $REPO
 cp Makefile_templates/Makefile_default Makefile
+mkdir -p etc
+cp templates/queue_config_${TwoPiDevice} etc/queue_config
+cp templates/slurm_batch_${TwoPiDevice} etc/slurm_batch
 
 export PetraM=${TwoPiRoot}
 

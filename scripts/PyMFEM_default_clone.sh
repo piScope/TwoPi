93a809f7397655ecb5405d69133b4e063883e1f1
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-07-11 17:14:50 -0400

    add options to PyMFEM and mfem clone script

--- scripts/PyMFEM_default_clone.sh
+++ scripts/PyMFEM_default_clone.sh
@@ -4,7 +4,44 @@ GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
 REPO="PyMFEM"
 
+DO_LATEST=true
+BRANCH=master
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -l|--latest)
+    DO_LATEST=true
+    shift # past argument    
+    ;;
+    -b|--branch)
+    BRANCH=$2
+    shift # past argument    
+    shift # past param
+    ;;
+    --sha)
+    SHA=$2
+    DO_LATEST=false	
+    shift # past argument
+    shift # past param
+esac
+done
+
+
 SC=$(dirname "$0")/subs/git_access.sh
 source $SC
 
 git_clone_or_pull "https://github.com/sshiraiwa/PyMFEM.git" $REPO $SRCDIR
+
+if [ ! -f ${SRCDIR}/${REPO} ]; then
+   cd ${SRCDIR}/${REPO}
+   echo "Checking out" ${BRANCH}   $DO_LATEST 
+   $GIT checkout ${BRANCH} 
+   if ! ${DO_LATEST} 
+   then   
+      echo "Checking out" ${SHA}
+      $GIT checkout ${SHA}
+   fi
+fi

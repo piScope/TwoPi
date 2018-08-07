657bbf63bb79b035ec39c6a33ceef7a95c91dc96
shiraiwa <shiraiwa@psfc.mit.edu>
2018-08-06 20:09:08 -0400

    working on scotch

--- scripts/scotch_default_build.sh
+++ scripts/scotch_default_build.sh
@@ -17,10 +17,18 @@ if [ ! -f $MAKEINC ]; then
    MAKEINC=$(dirname "$MYPATH")/../extra/scotch/scotch_default_Makefile.inc
 fi
 cp $MAKEINC ${REPO}/src/Makefile.inc
-cd ${REPO}
+cd ${REPO}/src
 
-$MAKE all
-$MAKE
+CCD="$CC -I$MPI_INCLUDE_PATH"
+echo ${CCD}
+$MAKE scotch CCS="$CC" CCP="$MPICC" CCD="$CCD"
+$MAKE ptscotch CCS="$CC" CCP="$MPICC" CCD="$CCD"
+$MAKE install prefix="$TwoPiRoot"
 
-
-
+CMAKELIST=$(dirname "$MYPATH")/../extra/scotch/CMakeLists.txt
+cp $CMAKELIST ${REPO}/src/CMakeLists.txt
+mkdir -p ${REPO}/src/cmbuild
+cd ${REPO}/src/cmbuild
+cmake .. -DCMAKE_INSTALL_NAME_DIR=${TwoPiRoot}/lib -DCMAKE_INSTALL_PREFIX=${TwoPiRoot}
+make VERBOSE=1
+make install

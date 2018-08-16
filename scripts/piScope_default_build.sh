9ee777a16f64ba84986b3bafc51da059b3fd71ef
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-08-16 11:48:57 -0400

    working on piScope installer

---
+++ scripts/piScope_default_build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pip install wxPython==4.0.3
+pip install matplotlib
+pip install Pillow
+pip install scipy
+pip install hgapi
+pip install PyOpenGL
+pip install netCDF4
+pip install PyPDF2
+pip install pdfrw
+pip install h5py
+pip install mpi4py
+
+

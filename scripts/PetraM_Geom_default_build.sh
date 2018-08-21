6c6f5d57d99f0e78bbb1f77757b57e9c508db715
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-08-21 09:45:58 -0400

    moved pygmsh install to build

--- scripts/PetraM_Geom_default_build.sh
+++ scripts/PetraM_Geom_default_build.sh
@@ -11,3 +11,16 @@ export PetraM=${TwoPiRoot}
 $MAKE
 $MAKE install
 
+## install older version of pygmsh from my fork
+## this one need == 0.5.0, and meshio==1.8.17, partly because fastfunc
+## fails on HomeBrew...
+DD=$PWD
+cd $SRCDIR
+$GIT clone https://github.com/sshiraiwa/pygmsh.git
+cd pygmsh
+python setup.py build
+python setup.py install --prefix=${TwoPiRoot}
+
+cd $DD
+
+
--- scripts/PetraM_Geom_default_clone.sh
+++ scripts/PetraM_Geom_default_clone.sh
@@ -9,16 +9,5 @@ source $SC
 
 git_clone_or_pull "${TwoPiGit}/PetraM_Geom.git" $REPO $SRCDIR
 
-## install older version of meshio and pygmsh
-## we ask voropy == 0.5.0, since a newer version depends on fastfunc which
-## fails on HomeBrew...
-
-cd $SRCDIR
-$GIT clone https://github.com/sshiraiwa/pygmsh.git
-cd pygmsh
-python setup.py build
-python setup.py install --prefix=${TwoPiRoot}
-
-cd $DD
 
 

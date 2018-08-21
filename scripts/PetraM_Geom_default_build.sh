5d59b116343893ec605cb78b5f8c9c2d04b63172
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-08-21 10:15:48 -0400

    working on ...Geom

--- scripts/PetraM_Geom_default_build.sh
+++ scripts/PetraM_Geom_default_build.sh
@@ -2,6 +2,7 @@
 
 SRCDIR=${TwoPiRoot}/src
 REPO=${SRCDIR}/PetraM_Geom
+DD=$PWD
 
 MAKE=$(command -v make)
 GIT=$(command -v git)
@@ -15,7 +16,7 @@ $MAKE install
 ## install older version of pygmsh from my fork
 ## this one need == 0.5.0, and meshio==1.8.17, partly because fastfunc
 ## fails on HomeBrew...
-DD=$PWD
+
 cd $SRCDIR
 $GIT clone https://github.com/sshiraiwa/pygmsh.git
 cd pygmsh

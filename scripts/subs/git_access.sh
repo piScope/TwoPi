a29ff70a5abf5bbb0ca45d6a47f7bdbc17760d58
shiraiwa <shiraiwa@psfc.mit.edu>
2018-10-10 16:38:26 -0400

    modified git_access.sh

--- scripts/subs/git_access.sh
+++ scripts/subs/git_access.sh
@@ -1,3 +1,21 @@
+function send_log() {
+   REPO=$1
+   H1="'Content-Type: application/json'"
+   ADDRESS="https://www-internal.psfc.mit.edu/~shiraiwa/petram_test/git_request.php"
+
+   DATA="'{\"repo\" : \""${REPO}"\"}'"
+   #
+   # make a command like this...
+   # wget -S --header=$H1  --post-data="{\"repo\" : \"PetraM_RF\"}" $ADDRESS
+   #
+   # verbose version
+   #  COMMAND="wget -S --header=${H1}  --post-data=${DATA}  ${ADDRESS}"
+   # silent version
+   COMMAND="wget --spider --header=${H1}  --post-data=${DATA}  ${ADDRESS} &> /dev/null"
+   #echo $COMMAND
+   eval $COMMAND
+}
+
 function git_clone_or_pull() {
     GITREPO=$1
     REPO=$2    
@@ -7,6 +25,9 @@ function git_clone_or_pull() {
     GIT=$(command -v git)
     mkdir -p $SRCDIR
     cd $SRCDIR
+
+    send_log $REPO
+    
     if [ ! -d $REPO ]; then
         $GIT clone $GITREPO $REPO    
         cd $SRCDIR/$REPO

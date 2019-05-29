9360d6293a9e2087e2de5b7932446cae7e68e8d3
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2019-05-29 08:36:36 -0400

    bump up MUMPS version number

--- scripts/MUMPS_default_build.sh
+++ scripts/MUMPS_default_build.sh
@@ -2,12 +2,14 @@
 
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
-REPO=${SRCDIR}/MUMPS_5.1.2
+
 CMAKE=$(command -v cmake)
 MAKE=$(command -v make)
 
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
 source $SCRIPT
+
+REPO=${SRCDIR}/${MUMPS_REPO}
 
 # find MPI 
 source $(dirname "$0")/subs/find_mpi.sh

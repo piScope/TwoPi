c51ab0f904a240aa244ffdfd58de9da8f07f8a08
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2019-08-18 20:42:55 -0400

    added --clean-swig and --run-swig in PetraM_MUMPS

--- scripts/PetraM_MUMPS_default_build.sh
+++ scripts/PetraM_MUMPS_default_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+MAKE=$(command -v make)
 SRCDIR=${TwoPiRoot}/src
 REPO=${SRCDIR}/PetraM_MUMPS
 
@@ -9,6 +9,43 @@ TWOPIINC=${TwoPiRoot}/include
 # load env
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
 source $SCRIPT
+
+
+DO_SWIG=false
+DO_CLEAN_SWIG=false
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --clean-swig)
+    DO_CLEAN_SWIG=true
+    shift # past argument    
+    ;;
+    --run-swig)
+    DO_SWIG=true
+    shift # past argument    
+    ;;
+    *)
+    echo "Unknown option " $key
+    exit 2  #  error_code=2
+    ;;
+esac
+done
+
+if $DO_SWIG ;then
+    cd ${REPO}    
+    MUMPS_INC=${SRCDIR}/${MUMPS_REPO}/include
+    export MUMPS_INC_DIR=${MUMPS_INC}
+    $MAKE cxx
+    exit 0
+fi
+if $DO_CLEAN_SWIG ;then
+    cd ${REPO}
+    MUMPS_INC=${SRCDIR}/${MUMPS_REPO}/include
+    $MAKE cleancxx
+    exit 0
+fi
 
 SCRIPT=$(dirname "$0")/buildcomponent.sh
 ${SCRIPT} PetraM_MUMPS build1

b8893d4b467752d21edb018abaf71c0ad31f74d9
shiraiwa <shiraiwa@psfc.mit.edu>
2019-11-20 17:08:14 -0500

    minor change

--- scripts/PUMI-PYTHON_default_build.sh
+++ scripts/PUMI-PYTHON_default_build.sh
@@ -40,3 +40,27 @@ cmake .. \
   -DENABLE_PYTHON=ON
 make 
 make install
+
+# add link to gmsh.py. it seems like there is two possible locations
+PYTHON=$TwoPiRoot/bin/python
+PYTHONVERSION=$(${PYTHON} -c "import os;print(os.path.basename(os.path.dirname(os.__file__))[-3:])")
+cd $TwoPiRoot/lib/python${PYTHONVERSION}/site-packages
+
+if [ -L pyCore.py ] ; then
+    echo "removing previous link to pyCore.py"
+    rm pyCore.py
+fi
+if [ -e pyCore.py ]; then
+    echo "(error) pyCore.py already exits under site-packages"
+    exit 1
+fi
+
+if [ -f $TwoPiRoot/lib/pyCore.py ]; then
+   ln -snf $TwoPiRoot/lib/pyCore.py pyCore.py
+   ln -snf $TwoPiRoot/lib/_pyCore.so _pyCore.so
+fi
+if [ -f $TwoPiRoot/lib64/pyCore.py ]; then
+   ln -snf $TwoPiRoot/lib64/pyCore.py pyCore.py
+   ln -snf $TwoPiRoot/lib64/_pyCore.so _pyCore.so
+fi
+

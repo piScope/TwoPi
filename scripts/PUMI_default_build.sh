a363ef9673a59f054f36f8cb03f60bc4d21f3538
shiraiwa <shiraiwa@psfc.mit.edu>
2019-11-20 13:19:23 -0500

    adding PUMI installer

--- scripts/PUMI_default_build.sh
+++ scripts/PUMI_default_build.sh
@@ -43,9 +43,9 @@ if $DO_TEST ;then
 	     -DSCOREC_CXX_OPTIMIZE=ON       \
              -DSCOREC_CXX_WARNINGS=ON       \
              -DCMAKE_INSTALL_PREFIX="${TwoPiRoot}" \
-#             -DBUILD_SHARED_LIBS=on   \
-#             -DCMAKE_VERBOSE_MAKEFILE=1 \
-#             -DIS_TESTING=ON \
+             -DBUILD_SHARED_LIBS=on   \
+             -DCMAKE_VERBOSE_MAKEFILE=1 \
+             -DIS_TESTING=ON \
 #	     -DENABLE_SIMMETRIX="${ENABLE_SIMMETRIX}" \	     
 #             -DMESHES="${TwoPiRoot}"/src/PUMI-MESHES \
              -DCMAKE_BUILD_TYPE="Debug" 

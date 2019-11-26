8cbbc81ddda3ec49f02db3bd4fc35354475b40b2
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2019-11-26 01:42:59 -0500

    add --clean-all in PetraM_MPW_default_build.sh

--- scripts/PetraM_MPW_default_build.sh
+++ scripts/PetraM_MPW_default_build.sh
@@ -15,11 +15,17 @@ export PetraM=${TwoPiRoot}
 DO_DEFAULT=true
 DO_SWIG=false
 DO_CLEAN_SWIG=false
+DO_CLEAN_ALL=false
 
 while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
+    --clean-all)
+    DO_CLEAN_ALL=true
+    DO_DEFAULT=false	
+    shift # past argument    
+    ;;
     --clean-swig)
     DO_CLEAN_SWIG=true
     DO_DEFAULT=false	
@@ -37,6 +43,9 @@ case $key in
 esac
 done
 
+if $DO_CLEAN_ALL ;then
+    $MAKE clean PREFIX=${TwoPiRoot} MPICC=${MPICC} MPICXX=${MPICXX} CC=${CC} CXX=${CXX}
+fi
 if $DO_CLEAN_SWIG ;then
     $MAKE clean_so PREFIX=${TwoPiRoot} MPICC=${MPICC} MPICXX=${MPICXX} CC=${CC} CXX=${CXX}
 fi

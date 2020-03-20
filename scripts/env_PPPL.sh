6cefe622a4b66848873d16a5387098b855381003
Shunichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-03-20 16:01:26 -0400

    update PPPL MUMPS

--- scripts/env_PPPL.sh
+++ scripts/env_PPPL.sh
@@ -18,7 +18,6 @@ source $(dirname $BASH_SOURCE)/env_common.sh
 # based on mkl link adviser, we should not need -lmkl_def
 # but without it, it causes mkl load error at run time
 export MUMPS_BLASLAPACK_FLAG="-L"${MKLROOT}" -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_def"
-#export MUMPS_SCALAPACK_FLAG="-L"${MKLROOT}"/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_intelmpi_lp64 -liomp5 -lpthread"
 export MUMPS_SCALAPACK_FLAG="-L"${MKLROOT}"/lib/intel64 -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64  -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core  -lmkl_def -liomp5 -lpthread -lm -ldl"
 
 # this flag controls if we use no-MPI MUMPS or not

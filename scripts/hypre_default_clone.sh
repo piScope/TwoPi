7425a1c0d5fd1e65d4138985ff0c1d0c1f92af17
Shunichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-03-29 12:54:42 -0400

    increment hypre version to 2.18.2

--- scripts/hypre_default_clone.sh
+++ scripts/hypre_default_clone.sh
@@ -6,6 +6,9 @@ SRCDIR=${TwoPiRoot}/src
 mkdir -p $SRCDIR
 cd $SRCDIR
 
+SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+source $SCRIPT
+
 wget https://github.com/hypre-space/hypre/archive/v${HYPRE_VERSION}.tar.gz -O hypre.tar.gz
 tar -zxvf hypre.tar.gz
 rm hypre.tar.gz

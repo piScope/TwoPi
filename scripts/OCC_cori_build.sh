002c6cf556de87bc0649c9daebd5ac5919af00a7
shiraiwa <shiraiwa@psfc.mit.edu>
2020-04-01 06:59:54 -0700

    add a special recipt for OCC cori

---
+++ scripts/OCC_cori_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GIT=$(command -v git)
+SRCDIR=${TwoPiRoot}/src
+REPO=${SRCDIR}/opencascade-7.4.0
+CMAKE=$(command -v cmake)
+MAKE=$(command -v make)
+
+SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+source $SCRIPT
+
+cd $REPO
+mkdir -p cmbuild
+cd cmbuild
+$CMAKE .. -DCMAKE_VERBOSE_MAKEFILE=1          \
+          -DCMAKE_INSTALL_PREFIX=${TwoPiRoot} \
+          -DCMAKE_C_COMPILER=${CC}            \
+          -DCMAKE_CXX_COMPILER=${CXX}         \
+	  -DBUILD_MODULE_Draw=0 \
+	  -DBUILD_MODULE_Visualization=0 \
+          -DCMAKE_INSTALL_NAME_DIR=${TwoPiRoot}/lib
+$MAKE $MAKEOPT
+$MAKE install

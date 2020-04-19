883f628bad622ade8e1d17cfd4b6aadbb26ef800
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-04-19 15:07:15 -0400

    bump up MUMPS to 5.3.1

--- scripts/hypre_default_clone.sh
+++ scripts/hypre_default_clone.sh
@@ -3,11 +3,11 @@
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
 
-mkdir -p $SRCDIR
-cd $SRCDIR
-
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
 source $SCRIPT
+
+mkdir -p $SRCDIR
+cd $SRCDIR
 
 wget https://github.com/hypre-space/hypre/archive/v${HYPRE_VERSION}.tar.gz -O hypre.tar.gz
 tar -zxvf hypre.tar.gz

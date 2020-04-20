3ad296b347d02a0706450b58b3d047caa837517f
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-04-20 11:59:58 -0400

    minor fix for brew

--- scripts/hypre_default_clone.sh
+++ scripts/hypre_default_clone.sh
@@ -4,7 +4,10 @@ GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
 
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+CMAKE_FIX_DIR=$(dirname "$0")/../extra/hypre
 source $SCRIPT
+
+ODIR=$PWD
 
 mkdir -p $SRCDIR
 cd $SRCDIR
@@ -15,7 +18,7 @@ rm hypre.tar.gz
 
 if [ ${HYPRE_VERSION} == "2.18.2" ]; then
     echo "Appling a fix to CMakeLists.txt"
-    CMAKE_FIX=$(dirname "$0")/../extra/hypre/CMakeLists_2.18.2_fix.txt
+    CMAKE_FIX=${CMAKE_FIX_DIR}/CMakeLists_2.18.2_fix.txt
     HYPREDIR=${TwoPiRoot}/src/hypre-${HYPRE_VERSION}/src/
     diff ${CMAKE_FIX} ${HYPREDIR}/CMakeLists.txt
     cp ${CMAKE_FIX} ${HYPREDIR}/CMakeLists.txt

2e8ef04eab719a88cb46458df85c195b465762b5
shiraiwa <shriaiwa@princeton.edu>
2020-05-07 14:18:21 -0400

    init copy activation_script and TwoPiGit is set to https://github.com/piScope

---
+++ scripts/env_ubuntu20.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CC=cc
+CXX=c++
+FC=gfortran
+MPICC=mpicc
+MPICXX=mpic++
+MPIFC=mpif90
+MPIFL=mpif90
+MAKEOPT="-j 4"
+OMPFLAG=-fopenmp
+OMPLINKFLAG=-fopenmp
+OMPCXXFLAG=-fopenmp
+OMPCCFLAG=-fopenmp
+OMPFCFLAG=-fopenmp
+
+source $(dirname $BASH_SOURCE)/env_common.sh
+#echo $MPI_INCLUDE_PATH
+
+

deea27cf00848629fd8b329eee2f13bd67bf1901
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-05-09 13:35:35 -0400

    finishe

--- scripts/mpi4py_default_build.sh
+++ scripts/mpi4py_default_build.sh
@@ -18,9 +18,9 @@ pip3 download --no-binary=:all: --dest . mpi4py==3.0.3
 tar -zxvf mpi4py-3.0.3.tar.gz
 cd mpi4py-3.0.3.tar.gz
 
-
 MPICC_FLAG=${MPICC} "-shared"
 echo ${MPICC_FLAG}
 
 cd mpi4py-3.0.3
 python setup.py build --mpicc="${MPICC_FLAG}"
+python seutp.py install PREFIX=${TwoPiRoot}

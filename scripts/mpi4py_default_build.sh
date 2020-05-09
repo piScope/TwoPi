35d47cf16cb51e585ee12f00d78ebcb9f4c99cf4
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-05-09 13:19:07 -0400

    added mpi4py clean

--- scripts/mpi4py_default_build.sh
+++ scripts/mpi4py_default_build.sh
@@ -19,6 +19,8 @@ tar -zxvf mpi4py-3.0.3.tar.gz
 cd mpi4py-3.0.3.tar.gz
 
 
-MPICC_FLAG=${MPICC} -shared
+MPICC_FLAG=${MPICC} "-shared"
+echo ${MPICC_FLAG}
 
+cd mpi4py-3.0.3
 python setup.py build --mpicc="${MPICC_FLAG}"

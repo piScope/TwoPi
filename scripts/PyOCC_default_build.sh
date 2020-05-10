26a354c3ce3830aca33e468bfaf815f1abdbffcb
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-05-10 14:11:42 -0400

    fixing build

--- scripts/PyOCC_default_build.sh
+++ scripts/PyOCC_default_build.sh
@@ -15,13 +15,15 @@ fi
 
 # if TwoPi has OOC installed use it otherwise defualt
 if [  -d ${TwoPiRoot}/include/opencascade ]; then
-    ${OCC_HEADER}=${TwoPiRoot}/include/opencascade 
-    ${OCC_LIB}=${TwoPiRoot}/lib
+    OCC_HEADER=${TwoPiRoot}/include/opencascade 
+    OCC_LIB=${TwoPiRoot}/lib
 else
-    ${OCC_HEADER}=/usr/local/include/opencascade
-    ${OCC_LIB}=/usr/local/lib
+    OCC_HEADER=/usr/local/include/opencascade
+    OCC_LIB=/usr/local/lib
 fi
 
+echo "OCC header :"${OCC_HEADER}
+echo "OCC lib    :"${OCC_LIB}
 mkdir -p $REPO/cmbuild
 cd $REPO/cmbuild
 $CMAKE .. -DCMAKE_INSTALL_PREFIX=${TwoPiRoot}     \

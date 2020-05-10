2a53f9d262b85dfd5c570ab0d5c03fb24bce910c
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-05-10 08:24:35 -0400

    added return code

--- scripts/OCC_default_clone.sh
+++ scripts/OCC_default_clone.sh
@@ -10,8 +10,15 @@ MYPATH=$BASH_SOURCE
 echo $MYPATH
 
 OOCTGZ=$(dirname "$MYPATH")/../externals/${OCC}.tar.gz
+if [ ! -f ${OOCTGZ} ];
+   OOCTGZ=$(dirname "$MYPATH")/../externals/${OCC}.tgz
+   if [ ! -f ${OOCTGZ} ];   
+       echo "OCC source tar.gz does not exists in "$(dirname "$MYPATH")/../externals
+       exit 1
+   fi
+fi
 
 cp $OOCTGZ $SRCDIR
 cd  $SRCDIR
 tar -zxvf ${OCC}.tar.gz
-rm $SRCDIR/${OCC}.tar.gz
+rm -rf $SRCDIR/${OCC}.tar.gz

4bfc78109e4748bfa5f0e19c2fbca8dba76c876b
Morteza HS <hakimi1364@gmail.com>
2020-06-13 15:23:14 -0400

    Updates to PUMI build script.
    Now the libraries and the python wrappers can be build all together.

--- scripts/PUMI_default_build.sh
+++ scripts/PUMI_default_build.sh
@@ -17,6 +17,8 @@ DO_TEST=false
 ENABLE_SIMMETRIX=OFF
 ENABLE_PARASOLID=OFF
 SIMMETRIX_LIB_DIR=''
+SIMMETRIX_MPI=openmpi
+ENABLE_PYTHON=OFF
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -34,6 +36,16 @@ case $key in
     SIMMETRIX_LIB_DIR=$2
     shift # past argument
     shift # past argument
+    ;;
+    --simmetrix-mpi)
+    SIMMETRIX_MPI=$2
+    shift # past argument
+    shift # past argument
+    ;;
+    --with-python)
+    ENABLE_PYTHON=ON
+    shift
+    shift
     ;;
     *)
     echo "Unknown option " $key
@@ -63,7 +75,7 @@ if $DO_TEST ;then
              -DCMAKE_CXX_COMPILER="${MPICXX}" \
              -DPUMI_FORTRAN_INTERFACE=OFF   \
 	     -DSCOREC_CXX_OPTIMIZE=ON       \
-             -DSCOREC_CXX_WARNINGS=ON       \
+             -DSCOREC_CXX_WARNINGS=OFF       \
              -DCMAKE_INSTALL_PREFIX="${TwoPiRoot}" \
              -DBUILD_SHARED_LIBS=on   \
              -DCMAKE_VERBOSE_MAKEFILE=1 \
@@ -71,25 +83,53 @@ if $DO_TEST ;then
 	     -DENABLE_SIMMETRIX="${ENABLE_SIMMETRIX}" \
              -DSIM_PARASOLID="${ENABLE_PARASOLID}"   \
 	     -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
-             -DSIM_MPI=openmpi3 \
+             -DSIM_MPI="${SIMMETRIX_MPI}" \
+             -DPUMI_PYTHON_INTERFACE="${ENABLE_PYTHON}" \
              -DMESHES="${TwoPiRoot}"/src/PUMI-MESHES \
+             -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-no-as-needed" \
+             -DCMAKE_EXE_LINKER_FLAGS="-Wl,-no-as-needed" \
              -DCMAKE_BUILD_TYPE="Debug" 
 else
     cmake .. -DSCOREC_CXX_WARNINGS=OFF   \
              -DCMAKE_C_COMPILER="${MPICC}"    \
              -DCMAKE_CXX_COMPILER="${MPICXX}" \
              -DPUMI_FORTRAN_INTERFACE=OFF   \
-	     -DSCOREC_CXX_OPTIMIZE=ON       \
-             -DSCOREC_CXX_WARNINGS=ON       \
+             -DSCOREC_CXX_OPTIMIZE=ON       \
+             -DSCOREC_CXX_WARNINGS=OFF       \
              -DCMAKE_INSTALL_PREFIX="${TwoPiRoot}" \
              -DBUILD_SHARED_LIBS=on   \
-             -DSIM_MPI=openmpi3 \
+             -DSIM_MPI="${SIMMETRIX_MPI}" \
              -DENABLE_SIMMETRIX="${ENABLE_SIMMETRIX}" \
              -DSIM_PARASOLID="${ENABLE_PARASOLID}"   \
-	     -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
-
+             -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
+             -DPUMI_PYTHON_INTERFACE="${ENABLE_PYTHON}" \
+             -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-no-as-needed" \
+             -DCMAKE_EXE_LINKER_FLAGS="-Wl,-no-as-needed" \
              -DCMAKE_VERBOSE_MAKEFILE=1 
 fi
 echo "${MAKEOPT}"
 make "${MAKEOPT}"
 make install
+
+# add link to pyCore.py. it seems like there is two possible locations
+PYTHON=$TwoPiRoot/bin/python
+PYTHONVERSION=$(${PYTHON} -c "import os;print(os.path.basename(os.path.dirname(os.__file__))[-3:])")
+cd $TwoPiRoot/lib/python${PYTHONVERSION}/site-packages
+
+if [ -L pyCore.py ] ; then
+    echo "removing previous link to pyCore.py"
+    rm pyCore.py
+fi
+if [ -e pyCore.py ]; then
+    echo "(error) pyCore.py already exits under site-packages"
+    exit 1
+fi
+
+if [ -f $TwoPiRoot/lib/pyCore.py ]; then
+   ln -snf $TwoPiRoot/lib/pyCore.py pyCore.py
+   ln -snf $TwoPiRoot/lib/_pyCore.so _pyCore.so
+fi
+if [ -f $TwoPiRoot/lib64/pyCore.py ]; then
+   ln -snf $TwoPiRoot/lib64/pyCore.py pyCore.py
+   ln -snf $TwoPiRoot/lib64/_pyCore.so _pyCore.so
+fi

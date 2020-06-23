a470143d9bb411702ad75585bbeac464efbad763
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-06-22 22:00:27 -0400

    minor fix

--- scripts/mpi4py_default_build.sh
+++ scripts/mpi4py_default_build.sh
@@ -16,7 +16,7 @@ cd ${WDIR}
 pip3 download --no-binary=:all: --dest . mpi4py==3.0.3
 
 tar -zxvf mpi4py-3.0.3.tar.gz
-cd mpi4py-3.0.3.tar.gz
+#cd mpi4py-3.0.3.tar.gz
 
 MPICC_FLAG=${MPICC} "-shared"
 echo ${MPICC_FLAG}

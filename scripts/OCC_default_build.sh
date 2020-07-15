45e79357a837f649bac4a130c06b81d93d11f290
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-07-15 16:03:16 -0400

    working on merge

--- scripts/OCC_default_build.sh
+++ scripts/OCC_default_build.sh
@@ -17,6 +17,7 @@ $CMAKE .. -DCMAKE_VERBOSE_MAKEFILE=1          \
           -DCMAKE_INSTALL_PREFIX=${TwoPiRoot} \
           -DCMAKE_C_COMPILER=${CC}            \
           -DCMAKE_CXX_COMPILER=${CXX}         \
+          -DCMAKE_CXX_FLAGS=$CXX11FLAG        \
           -DCMAKE_INSTALL_NAME_DIR=${TwoPiRoot}/lib
 $MAKE $MAKEOPT
 $MAKE install

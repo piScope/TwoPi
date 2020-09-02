c05895beece91fdc7d034017945249432d3e1dc3
Shunichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-09-02 12:44:29 -0400

    (WIP)addling lapack

---
+++ scripts/lapack_default_clone.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+GIT=$(command -v git)
+SRCDIR=${TwoPiRoot}/src
+
+SCRIPT_FULLPATH=$(realpath $(dirname "$0"))
+
+SCRIPT=${SCRIPT_FULLPATH}/env_${TwoPiDevice}.sh
+source $SCRIPT
+
+mkdir -p $SRCDIR
+cd $SRCDIR
+wget https://github.com/Reference-LAPACK/lapack/archive/v{LAPACK_VERSION}.tar.gz -O lapack.tar.gz
+tar -zxvf lapack.tar.gz
+rm lapack.tar.gz

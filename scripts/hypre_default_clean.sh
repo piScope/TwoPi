b1231e382d9b109e005daffe19564954a3276bee
Shunichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-09-02 13:03:13 -0400

    (WIP)addling lapack

--- scripts/hypre_default_clean.sh
+++ scripts/hypre_default_clean.sh
@@ -2,14 +2,10 @@
 
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
-HYPREDIR=${SRCDIR}/hypre-2.11.2
-CMAKE=$(command -v cmake)
-MAKE=$(command -v make)
 
 SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
 source $SCRIPT
 
-export CXX=${MPICXX}
-export CC=${MPICC}
+HYPREDIR=${SRCDIR}/hypre-${HYPRE_VERSION}
 
 rm -rf ${HYPREDIR}/src/cmbuild

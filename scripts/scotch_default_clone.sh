ba84113726f6a3112d667f7af8a3bf7ba6599e1f
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-10-10 19:51:42 -0400

    update clone script

--- scripts/scotch_default_clone.sh
+++ scripts/scotch_default_clone.sh
@@ -3,9 +3,14 @@
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
 
+# load env
+SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+source $SCRIPT
+
 mkdir -p $SRCDIR
 cd $SRCDIR
-wget https://gforge.inria.fr/frs/download.php/file/37622/scotch_6.0.6.tar.gz
-tar -zxvf scotch_6.0.6.tar.gz
-rm scotch_6.0.6.tar.gz
+wget https://gforge.inria.fr/frs/download.php/file/37622/scotch_${SCOTCH_VERSION}.tar.gz
+tar -zxvf ${SCOTCH_VERSION}.tar.gz
+rm scotch_${SCOTCH_VERSION}.tar.gz
+
 

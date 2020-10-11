0f1e590f8228b0e92385fe9f971ad84efe3e4a62
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-10-10 22:11:30 -0400

    added scotch_clean

---
+++ scripts/scotch_default_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT=$(command -v git)
+CMAKE=$(command -v cmake)
+MAKE=$(command -v make)
+SRCDIR=${TwoPiRoot}/src
+
+# load env
+SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+source $SCRIPT
+
+REPO=${SRCDIR}/scotch_${SCOTCH_VERSION}
+
+cd ${REPO}/src
+make clean

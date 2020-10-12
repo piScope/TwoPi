ae87143cba254222f7a320fef6bd55c9c75b56fb
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-10-12 11:02:44 -0400

    added mpi.h path in env_cori.sh

--- scripts/env_cori.sh
+++ scripts/env_cori.sh
@@ -18,5 +18,5 @@ source $(dirname $BASH_SOURCE)/env_common.sh
 # path to mpi.h
 #    (note) It appears find_mpi does not work with CC (cray MPI compiler)
 MPI_INCLUDE_PATH=/opt/cray/pe/mpt/7.7.10/gni/mpich-intel/16.0/include
-
+MPI_LIBRARY_PATH=/opt/cray/pe/mpt/7.7.10/gni/mpich-intel/16.0/lib
 
